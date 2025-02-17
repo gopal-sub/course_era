4943e03e0fe6304c0016cd28a1dbd0ed33833be0
Gopal Subs <gsubbaratinam@demandbase.com>
2025-02-18 00:45:15 +0530

    the skleton routes added

--- server.js
+++ server.js
@@ -3,7 +3,29 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.send("hello");
-})
+});
+
+app.get('/user/login', (res, res)=>{
+
+});
+app.get('/user/signup', (res, res)=>{
+    
+});
+app.get('/user/purchases', (res, res)=>{
+    
+});
+app.get('/courses', (res, res)=>{
+    
+});
+app.get('/courses/purchase', (res, res)=>{
+    
+});
+
+app.get('/courses', (res, res)=>{
+    
+});
+
+
 
 
 app.listen(3000, ()=>{
